ccdf0260957ca705e65dc3afa1f9a3dc04ed2d92
Dominik Pivka <64418326+AppleJamp@users.noreply.github.com>
2025-05-14 20:12:07 +0200

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  resources :projects
   resources :users
   root 'dashboard#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_110749) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_14_175419) do
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_110749) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "projects", "users"
 end
